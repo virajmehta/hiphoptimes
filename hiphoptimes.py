3f4c57334266560583bd24391aa821b9be909137
Viraj Mehta <thebigmehtaphor@gmail.com>
2015-12-22 10:47:15 -0600

    good good

--- hiphoptimes.py
+++ hiphoptimes.py
@@ -8,8 +8,9 @@ from bs4 import BeautifulSoup
 
 def main():
     while True:
-        #import pdb; pdb.set_trace()
-        x = requests.get('http://reddit.com/r/hiphopheads')
+        import pdb; pdb.set_trace()
+        headers = {'User-Agent': 'hasItDropped by 2miles4chicken'}
+        x = requests.get('http://reddit.com/r/hiphopheads', headers=headers)
         if x.status_code == 200:
             soup = BeautifulSoup(x.text, 'html.parser')
             stringUsers = soup.find(class_='users-online')
@@ -17,7 +18,7 @@ def main():
             with open('traffic.txt', 'a') as output:
                 output.write(time.strftime("%Y-%m-%d %H:%M") + '\n')
                 output.write(users + '\n')
-        time.sleep(600) #wait 10 minuntes before checking again
+        time.sleep(600) #wait 10 minutes before checking again
 
 
 if __name__ == '__main__':
